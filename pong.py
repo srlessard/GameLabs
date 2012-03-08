42097e352cdb8ccb37b024893ecfc7c0243d3cbf
Steven Lessard <sr.lessard@gmail.com>
2012-03-07 21:20:12 -0500

    Ball restarts in the middle of the screen after a score

--- pong.py
+++ pong.py
@@ -19,7 +19,8 @@ DIV_WIDTH = 5
 DIV_HEIGHT = SCREEN_HEIGHT
 DIV_START_X = SCREEN_WIDTH/2 - DIV_WIDTH/2
 DIV_START_Y = 0
-BALL_SPEED = 10
+BALL_SPEED_X = 10
+BALL_SPEED_Y = 10
 BALL_WIDTH_HEIGHT = 16
 POINTS_TO_WIN = 11
 
@@ -35,22 +36,15 @@ def load_sound(sound_name):
         raise SystemExit, message
     return sound
 
-# Collision sound
 collideSound = load_sound('laser.wav')
 
 # This is a rect that contains the ball at the beginning it is set in the center of the screen
 ball_rect = pygame.Rect((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), (BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT))
+ball_speed = [BALL_SPEED_X, BALL_SPEED_Y]
 
-# Speed of the ball (x, y)
-ball_speed = [BALL_SPEED, BALL_SPEED]
-
-# P1 paddle vertically centered on the left side
 paddle1_rect = pygame.Rect((PADDLE1_START_X, PADDLE_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
-
-# P2 paddle vertically centered on the right side
 paddle2_rect = pygame.Rect((PADDLE2_START_X, PADDLE_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
 
-# Screen Divider
 div_rect = pygame.Rect((DIV_START_X, DIV_START_Y), (DIV_WIDTH, DIV_HEIGHT))
 
 # Scoring: 1 point if you hit the ball, -5 point if you miss the ball
@@ -83,15 +77,15 @@ while True:
 
     # P1 Controls
     if pygame.key.get_pressed()[pygame.K_w] and paddle1_rect.top > 0:
-        paddle1_rect.top -= BALL_SPEED
+        paddle1_rect.top -= BALL_SPEED_Y
     elif pygame.key.get_pressed()[pygame.K_s] and paddle1_rect.bottom < SCREEN_HEIGHT:
-        paddle1_rect.top += BALL_SPEED
+        paddle1_rect.top += BALL_SPEED_Y
 
     # P2 Controls
     if pygame.key.get_pressed()[pygame.K_UP] and paddle2_rect.top > 0:
-        paddle2_rect.top -= BALL_SPEED
+        paddle2_rect.top -= BALL_SPEED_Y
     elif pygame.key.get_pressed()[pygame.K_DOWN] and paddle2_rect.bottom < SCREEN_HEIGHT:
-        paddle2_rect.top += BALL_SPEED
+        paddle2_rect.top += BALL_SPEED_Y
 
     # Quit Button    
     if pygame.key.get_pressed()[pygame.K_ESCAPE]:
@@ -106,11 +100,13 @@ while True:
     if ball_rect.top <= 0 or ball_rect.bottom >= SCREEN_HEIGHT:
         ball_speed[1] = -ball_speed[1]
     if ball_rect.right >= SCREEN_WIDTH: # Player 2 scores
-        ball_speed[0] = -ball_speed[0]
         p2Score += 1
+        ball_rect = pygame.Rect((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), (BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT))
+        ball_speed = [-BALL_SPEED_X, BALL_SPEED_Y]
     if ball_rect.left <= 0:             # Player 1 scores
-        ball_speed[0] = -ball_speed[0]
         p1Score += 1
+        ball_rect = pygame.Rect((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), (BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT))
+        ball_speed = [BALL_SPEED_X, BALL_SPEED_Y]
 
     if p1Score > (POINTS_TO_WIN - 1) or p2Score > (POINTS_TO_WIN - 1) : # Winning conditions
         cont = True
